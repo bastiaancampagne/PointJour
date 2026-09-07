b59b993ed17fbeca72be3a8039a04a4e015b6606
bastiaancampagne <bastiaan.campagne@gmail.com>
2026-09-07 19:39:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pointjour-v3-warm-20260907';
+const CACHE='pointjour-v4-dual-20260907';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./config.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./assets/emu-breakfast.png','./assets/emu-brief.png','./assets/emu-watches.png','./assets/emu-search.png','./assets/emu-experts.png','./assets/emu-loading.png','./data/news.json'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
