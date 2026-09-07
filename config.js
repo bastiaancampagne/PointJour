bac6fbd508e5b66319f7d1aaceb96dde556c6c60
bastiaancampagne <bastiaan.campagne@gmail.com>
2026-09-07 18:40:11 +0200

    Update GOOGLE_CLIENT_ID in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.POINTJOUR_CONFIG = {
-  GOOGLE_CLIENT_ID: "112001201855-rl61a4mk012fe0hfmtdp52v04l4rco0p.apps.googleusercontent.com",
+  GOOGLE_CLIENT_ID: "112001201855-r161a4mk012fe0hfmtdp52v0414rco0p.apps.googleusercontent.com",
   PAYROLL_NEWS_URL: "https://raw.githubusercontent.com/bastiaancampagne/VeilleJurSoc/main/data/news.json"
 };
