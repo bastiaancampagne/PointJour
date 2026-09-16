96f33c0a9e30fb1add00648cad2cf683edb22964
bastiaancampagne <bastiaan.campagne@gmail.com>
2026-09-16 21:06:56 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.POINTJOUR_CONFIG = {
+  GOOGLE_CLIENT_ID: "112001201855-rl61a4mk012fe0hfmtdp52v04l4rco0p.apps.googleusercontent.com",
+  PAYROLL_NEWS_URL: "https://raw.githubusercontent.com/bastiaancampagne/VeilleJurSoc/main/data/news.json"
+};
